7818119fb0b96ca847165d4c18aa772a73019723
Droped17 <droped17@gmail.com>
2023-08-31 11:03:26 +0700

    state: condition render in addTask

--- src/components/Todo/TodoCreate.jsx
+++ src/components/Todo/TodoCreate.jsx
@@ -1,15 +1,83 @@
-import styles from './TodoCreate.module.scss';
-import { FaPlus } from 'react-icons/fa';
-import { HiPlus } from 'react-icons/hi';
+import { useState } from "react";
+import { FaPlus } from "react-icons/fa";
+import { HiPlus } from "react-icons/hi";
+
+
+import TodoForm from "./TodoForm";
+import styles from "./TodoCreate.module.scss";
+
+/* 
+  Condition Rendering
+  -Default : Show button & Text
+  -Active : Show TodoForm
+
+  Concept: true ? <AddTask/> : <TodoForm></TodoForm>
+*/
+
+/* 
+  CC2 - Event Handling
+  เอา fn ไปผูกกับ UI เพื่อให้ user เป็นคนเรียกใช้เอง
+  - onClick : ต้อง click ก่อน fn ถึงจะ run
+  - Browser จะเรียกใช้ fn แล้วโยน eventObj มาให้
+      handleClick(eventObject)
+  
+*/
+
+/* 
+  Array Destructoring
+
+  function myUseState(){
+    return [5,9];
+  }
+
+  let [a,b] = myUseState()
+  a == 5
+  b == 9
+
+*/
+
+
+/* 
+  CC3 - Js value ไม่สามารถทำให้ react re-render ได้
+  ต้องใช้ state
+
+  ReactState เป็น 1 ใน fn ของ ReactHook
+  const [state,setState] = userState(initialState:any)
+
+*/
+
 
 function TodoCreate() {
+        //state    setState
+ const [isOpenForm,setIsOpenForm] = useState(false);
+ 
+  // let active = false; 
+
+  const handleClick = function (event){
+    // console.log(event); 
+    // active = !active;
+    // console.log('click',active);
+
+    setIsOpenForm(!isOpenForm);
+
+    
+  };
+
+
+
   return (
-    <div className={styles.todo__create}>
-      <div className={styles.todo__create__button}>
-        <HiPlus />
-      </div>
-      <h3 className={styles.todo__create__text}>Add Task</h3>
-    </div>
+    <>
+      {isOpenForm ? (
+        <TodoForm></TodoForm>
+      ) : (
+        <div onClick={handleClick} className={styles.todo__create}>
+          <div className={styles.todo__create__button}>
+            <HiPlus />
+          </div>
+          <h3 className={styles.todo__create__text}>Add Task</h3>
+        </div>
+      )}
+    </>
   );
 }
 
