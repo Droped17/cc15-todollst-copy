dc42e0b1b40e816ca6514ba235219182c555d871
Droped17 <droped17@gmail.com>
2023-08-31 16:48:49 +0700

    pass props from tolist to todoitem

---
+++ src/components/Todo/TodoItem.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { FaTrashAlt, FaPen } from "react-icons/fa";
+import { HiOutlineCheck } from "react-icons/hi";
+import styles from "./TodoItem.module.scss";
+import TodoForm from "./TodoForm";
+
+function TodoItem({task,done,date}){ //วิธี 2 ** Recommend
+
+    // const { task, done, date} = props; วิธี 1
+
+    const [isOpenEditMode, setIsOpenEditMode] = useState(false);
+
+    const handleClickEdit = function () {
+      console.log(isOpenEditMode);
+  
+      setIsOpenEditMode(!isOpenEditMode);
+    };
+    return (
+        <>
+            {isOpenEditMode ? (
+          <TodoForm  textSubmit="Edit Task" setIsOpenForm={setIsOpenEditMode}></TodoForm>
+        ) : (
+          <li className={styles.todo}>
+            <div
+              className={`${styles.todo__checkbox} ${done ? styles.todo__checkbox__done : ''}`}
+            >
+              <HiOutlineCheck className={styles.todo__checkbox__icon} />
+            </div>
+            <p className={`${styles.todo__task} ${done ? styles.todo__task__done : ''}`}>
+              {task}
+            </p>
+            <span className={styles.todo__date}>{date}</span>
+            <div className={styles.todo__action}>
+              <span onClick={handleClickEdit}>
+                <FaPen className={styles.todo__edit} />
+              </span>
+              <span>
+                <FaTrashAlt className={styles.todo__delete} />
+              </span>
+            </div>
+          </li>
+        )}
+        </>
+    );
+}
+
+export default TodoItem;
--- src/components/Todo/TodoLists.jsx
+++ src/components/Todo/TodoLists.jsx
@@ -1,45 +1,16 @@
-import { useState } from "react";
-import { FaTrashAlt, FaPen } from "react-icons/fa";
-import { HiOutlineCheck } from "react-icons/hi";
 
+import TodoItem from "./TodoItem";
 import styles from "./TodoLists.module.scss";
-import TodoForm from "./TodoForm";
 
 function TodoLists() {
-  const [isOpenEditMode, setIsOpenEditMode] = useState(false);
-
-  const handleClickEdit = function () {
-    console.log(isOpenEditMode);
-
-    setIsOpenEditMode(!isOpenEditMode);
-  };
 
   return (
     <>
       <ul className={styles.todo__lists}>
-        {isOpenEditMode ? (
-          <TodoForm  textSubmit="Edit Task" setIsOpenForm={setIsOpenEditMode}></TodoForm>
-        ) : (
-          <li className={styles.todo}>
-            <div
-              className={`${styles.todo__checkbox} ${styles.todo__checkbox__done}`}
-            >
-              <HiOutlineCheck className={styles.todo__checkbox__icon} />
-            </div>
-            <p className={`${styles.todo__task} ${styles.todo__task__done}`}>
-              todo-item 1{" "}
-            </p>
-            <span className={styles.todo__date}>30 Aug</span>
-            <div className={styles.todo__action}>
-              <span onClick={handleClickEdit}>
-                <FaPen className={styles.todo__edit} />
-              </span>
-              <span>
-                <FaTrashAlt className={styles.todo__delete} />
-              </span>
-            </div>
-          </li>
-        )}
+          <TodoItem task="Do HW" done={true} date='31 Aug'></TodoItem>
+          <TodoItem task="Do HW2" done={false} date='31 Aug'></TodoItem>
+          <TodoItem task="Do HW3" done={true} date='31 Aug'></TodoItem>
+          <TodoItem task="Do HW4" done={false} date='31 Aug'></TodoItem>
       </ul>
     </>
   );
